95bd8da1733793aed235f8b5327c59570b163fd6
tero-yrjola <yrjola.tero@gmail.com>
2020-03-24 19:37:40 +0200

    Display the amount of players until start

--- app.js
+++ app.js
@@ -24,7 +24,10 @@ io.on("connection", socket => {
         rooms.push(gameIndex);
         io.to(gameIndex).emit('setGameRoom', gameIndex);
         waitingClients = [];
-    } else waitingClients.push(socket);
+    } else {
+        waitingClients.push(socket);
+        waitingClients.forEach(c => io.to(c.id).emit('playersUntilStart', playerAmount - waitingClients.length))
+    }
 
     socket.on('updateText', (newText) => {
         const rooms = socket.rooms && Object.keys(socket.rooms).filter(roomName => roomName !== socket.id);
@@ -40,6 +43,7 @@ io.on("connection", socket => {
         console.log("Client " + socket.id + " disconnected.");
         if (waitingClients.map(c => c.id).includes(socket.id))
             waitingClients.splice(waitingClients.map(c => c.id).indexOf(socket.id), 1);
+        waitingClients.forEach(c => io.to(c.id).emit('playersUntilStart', playerAmount - waitingClients.length));
     });
 });
 
